93ea58b061bc162cf6f87cf35e36f708025ff10c
aberkow <berkowitz.clarinet@gmail.com>
2016-02-12 14:42:07 -0500

    fixed loading on page problem

--- app.js
+++ app.js
@@ -1,24 +1,50 @@
+var userNumber;
+
 $(document).ready(function() {
-    $('button').on('click', fizzbuzz);  
+    $('#user_input').change(function() {
+        debugger;
+        var numberAsString = $('.number').val();
+        userNumber = parseInt(numberAsString, 10);
+    });
+    
+    $('#form').submit(function(evt) {
+        fizzbuzz(userNumber); //do fizzbuzz on userNumber
+        evt.preventDefault();
+    });
 });
 
-
-function fizzbuzz() {
-    for (var i = 1; i <= 100; i++) {
-        if ((i % 3 == 0) && (i % 5 == 0)) {
-            $('.fizzbuzz').append('<div>"fizzbuzz"</div>');
-        }
-        else if (i % 3 == 0) {
-            $('.fizzbuzz').append('<div>"fizz"</div>');
-        }
-        else if (i % 5 == 0) {
-            $('.fizzbuzz').append('<div>"buzz"</div>');
-        }
-        else {
-            $('.fizzbuzz').append('<div>' + i + '</div>');
-        }
+function fizzbuzz(input) {
+    
+    if (input == NaN) { //check to see if the input is NaN and alert
+        alert('Please enter a number.'); 
     }
+    else { //play fizzbuzz until reaching the input number
+        for (var i = 1; i <= input; i++) {
+   
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                $('.fizzbuzz').append('<div>"fizzbuzz"</div>');
+                console.log('fizzbuzz');
+                }
+            else if (i % 3 == 0) {
+                $('.fizzbuzz').append('<div>"fizz"</div>');
+                console.log('fizz');
+                }
+            else if (i % 5 == 0) {
+                $('.fizzbuzz').append('<div>"buzz"</div>');
+                console.log('buzz');
+                }
+            else {
+                $('.fizzbuzz').append('<div>' + i + '</div>')
+                console.log(i);
+            }
+            
+        }
+        
+    }   
 }
+
+
+    
 
 
     
