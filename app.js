c995a9c1f2e1ff6105a5674a2c3d8388496b356b
aberkow <berkowitz.clarinet@gmail.com>
2016-02-11 21:15:02 -0500

    all set

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 $(document).ready(function() {
-    console.log('hello');
-    
-    $('button').on('click', fizzbuzz);
-    
+    $('button').on('click', fizzbuzz);  
 });
 
 
 function fizzbuzz() {
     for (var i = 1; i <= 100; i++) {
         if ((i % 3 == 0) && (i % 5 == 0)) {
-            $('body').append('fizzbuzz' + '\r');
+            $('.fizzbuzz').append('fizzbuzz ' + '\r');
         }
         else if (i % 3 == 0) {
-            $('body').append('fizz' + '\r');
+            $('.fizzbuzz').append('fizz ' + '\r');
         }
         else if (i % 5 == 0) {
-            $('body').append('buzz' + '\r');
+            $('.fizzbuzz').append('buzz ' + '\r');
         }
         else {
-            $('body').append(i + '\r');
+            $('.fizzbuzz').append(i + ' \r');
         }
     }
 }
